fdc13cc132bf9e43062694bd5ebb2d8047185a86
k2a3r5 <isabela.karczis@yahoo.com>
2026-06-20 13:01:34 +0300

    Add files via upload

---
+++ twig_init.php
@@ -0,0 +1,11 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+$loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/templates');
+$twig = new \Twig\Environment($loader, [
+    'cache' => false,
+    'debug' => true,
+]);
+
+return $twig;
+?>
